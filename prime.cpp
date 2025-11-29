592e85191f08d8a0c48f9bed1b1d46cd7b5e26e4
yanagi-lpc <221145973+yanagi-lpc@users.noreply.github.com>
2025-11-30 03:02:04 +0900

    C++に移植

---
+++ prime.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int inputnum;
+    cin >> inputnum;
+
+    auto start = chrono::steady_clock::now();
+
+    vector<int> sieve(inputnum + 1, 1);
+    sieve[0] = sieve[1] = 0;
+    int count = 0;
+    
+    for (int i = 3; i <= sqrt(inputnum); i += 2){
+        if (sieve[i] == 1){
+            for (int j = i * i; j <= inputnum; j += 2 * i){
+                sieve[j] =0;
+            }
+        }
+    }
+
+    auto end = chrono::steady_clock::now();
+    auto time = end - start;
+
+    cout << 2 << " ";
+    count++;
+
+    for (int i = 3; i <= inputnum; i += 2){
+        if (sieve[i] == 1){
+            cout << i << " ";
+            count++;
+        }
+    } 
+    cout << endl;
+
+
+    auto msec = chrono::duration_cast<chrono::milliseconds>(time).count();
+    cout << "ˆ—ŠÔ: " << msec << "ms" << endl;
+
+    cout << "‘f”‚ÌŒÂ”: " << count << endl;
+
+}
